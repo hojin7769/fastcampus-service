93c7adb94708c1c2aa86e9495bd205d5d0d63c80
HoJin <hojin7769>
2024-04-15 21:32:06 +0900

    common 옮기기

--- common/src/main/kotlin/org/delivery/common/api/Api.kt
+++ common/src/main/kotlin/org/delivery/common/api/Api.kt
@@ -1,11 +1,53 @@
 package org.delivery.common.api
 
-import lombok.AllArgsConstructor
-import lombok.Data
-import lombok.NoArgsConstructor
+import org.delivery.common.error.ErrorCodeIfs
+import javax.validation.Valid
 
-data class Api<T> (
-    var result: Result?= null,
 
-    var body: T?= null
-)
+data class Api<T>(
+    var result: Result? = null,
+
+    @field:Valid
+    var body: T? = null
+) {
+
+    companion object{
+
+        @JvmStatic
+        fun <T> OK(body: T?): Api<T> {
+            return Api(
+                result = Result.OK(),
+                body = body
+            )
+        }
+        @JvmStatic
+        fun <T> ERROR(result: Result): Api<T> {
+            return Api(
+                result = result,
+            )
+        }
+
+        @JvmStatic
+        fun <T> ERROR(errorCodeIfs: ErrorCodeIfs): Api<T> {
+            return Api(
+                result = Result.ERROR(errorCodeIfs),
+            )
+        }
+
+        @JvmStatic
+        fun <T> ERROR(errorCodeIfs: ErrorCodeIfs, cause: Throwable): Api<T> {
+            return Api(
+                result = Result.ERROR(errorCodeIfs, cause),
+            )
+        }
+
+        @JvmStatic
+        fun <T> ERROR(errorCodeIfs: ErrorCodeIfs, description: String): Api<T> {
+            return Api(
+                result = Result.ERROR(errorCodeIfs, description),
+            )
+        }
+
+
+    }
+}
--- common/src/main/kotlin/org/delivery/common/api/Result.kt
+++ common/src/main/kotlin/org/delivery/common/api/Result.kt
@@ -1,4 +1,59 @@
 package org.delivery.common.api
 
-class Result {
+import org.delivery.common.error.ErrorCode
+import org.delivery.common.error.ErrorCodeIfs
+
+
+class Result(
+    val resultCode : Int?=null,
+    val resultMessage: String?=null,
+    val resultDescription: String?=null
+) {
+
+    companion object{
+
+        @JvmStatic
+        fun OK(): Result{
+            return Result(
+                resultCode = ErrorCode.OK.getErrorCode(),
+                resultMessage = ErrorCode.OK.getDescription(),
+                resultDescription = "성공"
+            )
+        }
+
+        @JvmStatic
+        fun ERROR(errorCodeIfs: ErrorCodeIfs): Result{
+            return Result(
+                resultCode = errorCodeIfs.getErrorCode(),
+                resultMessage = errorCodeIfs.getDescription(),
+                resultDescription = "에러발생"
+            )
+        }
+
+        @JvmStatic
+        fun ERROR(
+            errorCodeIfs: ErrorCodeIfs,
+            cause:Throwable
+        ): Result{
+            return Result(
+                resultCode = errorCodeIfs.getErrorCode(),
+                resultMessage = errorCodeIfs.getDescription(),
+                resultDescription = cause.localizedMessage
+            )
+        }
+
+        @JvmStatic
+        fun ERROR(
+            errorCodeIfs: ErrorCodeIfs,
+            description:String
+        ): Result{
+            return Result(
+                resultCode = errorCodeIfs.getErrorCode(),
+                resultMessage = errorCodeIfs.getDescription(),
+                resultDescription = description
+            )
+        }
+
+
+    }
 }
