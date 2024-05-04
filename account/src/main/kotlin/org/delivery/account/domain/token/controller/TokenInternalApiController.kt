360f4ba0480c15d92658946b138963ea2a7e8d4e
HoJin <hojin7769>
2024-05-05 00:30:37 +0900

    refactor: 기존 프로젝트를 Kotlin으로 변경하기 - 8 비즈니스 로직 변경
     8 비즈니스 로직 변경

---
+++ account/src/main/kotlin/org/delivery/account/domain/token/controller/TokenInternalApiController.kt
@@ -0,0 +1,29 @@
+package org.delivery.account.domain.token.controller
+
+import org.delivery.account.domain.token.business.TokenBusiness
+import org.delivery.account.domain.token.controller.model.TokenValidationRequest
+import org.delivery.account.domain.token.controller.model.TokenValidationResponse
+import org.delivery.common.annotation.Business
+import org.delivery.common.api.Api
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/internal-api/token")
+class TokenInternalApiController(
+    private val tokenBusiness: TokenBusiness
+) {
+
+    @PostMapping("/validation")
+    fun tokenValidation(
+        @RequestBody
+        tokenValidationRequest: TokenValidationRequest?
+    ):Api<TokenValidationResponse>{
+        return Api.OK(
+            tokenBusiness.tokenValidation(tokenValidationRequest)
+        )
+    }
+
+}
