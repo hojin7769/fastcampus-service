f043aa99321c97b1dfc15a17416e10804c8365ab
HoJin <hojin7769>
2024-05-07 21:13:47 +0900

    feat: apigw 생성 및 인증feat: apigw 생성 및 인증 절차 생성 절차 생성

--- api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
+++ api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
@@ -1,12 +1,15 @@
 package org.delivery.api.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.delivery.common.api.Api;
 import org.delivery.api.domain.token.controller.model.TokenResponse;
 import org.delivery.api.domain.user.business.UserBusiness;
 import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
+import org.delivery.common.error.ErrorCode;
+import org.delivery.common.exception.ApiException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/open-api/user")
@@ -36,6 +40,7 @@ public class UserOpenApiController {
             @Valid
             @RequestBody Api<UserLoginRequest> request
     ){
+//        if(true) throw new ApiException(ErrorCode)
         var response = userBusiness.login(request.getBody());
         return Api.OK(response);
     }
